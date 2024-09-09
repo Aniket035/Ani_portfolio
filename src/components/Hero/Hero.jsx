7ff511256bb6055840f4b253d6dfc5d8ce8887c3
Aniket035 <118649948+Aniket035@users.noreply.github.com>
2024-09-09 10:55:12 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import styles from "src/components/Hero/Hero.module.css";
+import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
