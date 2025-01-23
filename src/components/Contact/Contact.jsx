c4602740d8acbca23b964001e3ddd13d41b0debe
Aniket035 <rajaniket035@gmail.com>
2025-01-24 02:14:28 +0530

    commit all

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:rajaniket035@gmail.com">rajaniket035@gmail.com</a>
+          <a href="mailto:rajaniket035@gmail.com" target="_blank">rajaniket035@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/aniket-kumar-3a1aa3231/">linkedin.com/aniket035</a>
+          <a href="https://www.linkedin.com/in/aniket-kumar-3a1aa3231/" target="_blank">linkedin.com/aniket035</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/Aniket035">github.com/Aniket035</a>
+          <a href="https://www.github.com/Aniket035" target="_blank">github.com/Aniket035</a>
         </li>
         
         
